fa8c8c9d368c7c26f8a56da032fea587b26149b0
ahsyuyu <ahsyuyu@gmail.com>
2015-03-12 10:12:50 +0800

    update to ksana 2015

---
+++ adarsha/ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 345,
+ "title": "adarsha",
+ "minruntime": 1.3,
+ "baseurl": "http:/rawgit.com/karmapa/adarsha/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "bundle.js",
+  "jquery.js",
+  "react-with-addons.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "jiangkangyur.kdb"
+ ],
+ "filesizes": [
+  584,
+  5162,
+  3288819,
+  268382,
+  625422,
+  847,
+  35601,
+  113498,
+  151747648
+ ],
+ "filedates": [
+  "2015-02-10T07:55:35.000Z",
+  "2015-03-05T07:10:55.000Z",
+  "2015-03-05T08:37:27.000Z",
+  "2015-02-10T01:58:08.000Z",
+  "2015-02-10T01:58:08.000Z",
+  "2015-03-05T08:37:06.000Z",
+  "2015-02-10T01:58:08.000Z",
+  "2015-02-10T01:58:08.000Z",
+  "2015-02-10T03:09:18.000Z"
+ ],
+ "date": "2015-03-05T08:37:27.958Z"
+})
